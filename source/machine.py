b703f995fc47aa18f58b2921693244f53a701598
discpic-ron <granddriod1210@gmail.com>
2026-02-07 14:01:16 -0500

    Implement update logic for machine processing

--- source/machine.py
+++ source/machine.py
@@ -1,7 +1,29 @@
+from recipes import Recipe
+
 class machine:
   def __init__(self,recipe):
     self.progress = 0
     self.recipe = recipe # this uses a dict
+    self.is_working = False
     
-  def update(self):
-    pass
+  def update(self,dt):
+    if not self.is_working:
+      if self.can_start():
+        self.is_working = True
+        self.progress = 0
+        
+    if self.is_working:
+      self.progress += dt / self.recipe["time"]
+      
+    if self.progress >= 1.0:
+      self.complete_craft()
+      
+  def can_start(self):
+    if Recipe.has_ingridents() == True:
+      return True
+    else:
+      return False
+      
+  def complete_craft(self):
+    self.progress = 0
+    self.is_working = False
